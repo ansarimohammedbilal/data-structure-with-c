725007fb8455bd73f46cecd362500f2e42f18275
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-21 20:33:52 +0530

    addition/substract/multiply/divison

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main() {
+    int y = 2;
+    int x = 2;
+    int sum = y+x;
+    int multiple = y*x ;
+    int substract= y-x;
+    int division = y/x;
+    printf("%d\n", division);
+    printf("%d\n" ,substract);
+    printf("%d\n" ,multiple);
+    printf("%d\n" ,sum);
+return 0 ;
+}
