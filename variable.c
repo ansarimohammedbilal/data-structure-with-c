f2bfa837cf5d74d9d94b14f2cb057ed0d500dfeb
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-21 18:24:46 +0530

    using variable

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+   int main() {
+    printf ("my name is - bilal\n");
+        int myage = 19;
+        printf ("my age is :%d\n",myage);
+        printf("my friend name is - talha\n")
+        ;int myfriendage = 20;
+        printf("friend age is :%d\n",myfriendage);
+        return 0;
+}
