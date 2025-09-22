43c6b208ac2c47f508031e6831c63e15f4aaa60d
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-22 16:14:53 +0530

    using number

---
+++ number.c
@@ -0,0 +1,10 @@
+#include  <stdio.h>
+int main(){
+    int age = 19;
+    float weight= 70.0f;
+    double height = 5.5;
+    printf("%d\n",age);
+    printf("%f\n",weight); 
+    printf("%lf\n",height);
+return 0;
+}
