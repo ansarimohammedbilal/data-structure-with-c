a49ad6e4d368e5ae79f157db0a38b2967efd6cd8
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-24 18:59:35 +0530

    real-life-example-2

---
+++ example2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int peopleinroom=0;
+    peopleinroom++;
+    peopleinroom++;
+    printf("%d\n",peopleinroom);
+    peopleinroom--;
+    printf("%d\n",peopleinroom);
+    return 0;
+
+
+}
