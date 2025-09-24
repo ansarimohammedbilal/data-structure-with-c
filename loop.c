421d69f05ad5f72c97e80e09df625a1857a90383
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-25 00:56:41 +0530

    loop-countdown-example

---
+++ loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    int countdown= 3;
+    while (countdown >0) {
+    printf("%d\n",countdown);
+        countdown--;
+    }
+    printf ("happy new year!!!\n");
+    return 0;
+}
