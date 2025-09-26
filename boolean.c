0fdff51d78b3d5c0edc6617fc6449a1c16f1a463
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-09-26 23:53:27 +0530

    using-boolean

---
+++ boolean.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main (){
+  bool isProgrammingFun = true;
+  bool isFishTasty = false;
+  printf("%d\n", isProgrammingFun);  // Returns 1 (true)
+  printf("%d", isFishTasty);         // Returns 0 (false)
+  
+    return 0;
+}
