172bbf315af572f037abbbde3f44bba6f8947048
ansarimohammedbilal <ansaribilal5p@gmail.com>
2025-10-13 19:33:26 +0530

    array-loop

---
+++ array-loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int mynumber []={5,10,15,20,25};
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+      printf("%d\n",mynumber[i]);  
+    }
+     return 0;
+}
